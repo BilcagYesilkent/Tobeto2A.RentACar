c10ca731f528736bf6936f191f29a9c217c56a33
bilcag yesilkent <bilcag.yesilkent@gmail.com>
2024-03-01 18:05:39 +0300

    Add project files.

---
+++ src/tobeto2A.RentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
+
+namespace Application.Services.Repositories;
+public interface IBrandRepository : IAsyncRepository<Brand, Guid>, IRepository<Brand, Guid>
+{
+
+}
